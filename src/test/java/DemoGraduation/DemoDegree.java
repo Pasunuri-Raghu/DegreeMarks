cfc32211880bf5226af1e31d640c386f0014da3f
Pasunuri-Raghu <psnr.raghu@gmail.com>
2025-01-29 15:34:04 +0530

    first commit

---
+++ src/test/java/DemoGraduation/DemoDegree.java
@@ -0,0 +1,10 @@
+package DemoGraduation;
+
+public class DemoDegree {
+
+	public static void main(String[] args) {
+System.out.println("This is raghu from warangal,i send my marks sheet 786");
+
+	}
+
+}
